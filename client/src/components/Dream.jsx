31ae8cc44ea055c100a2a221ee545bc5b8af1b4f
Charan Karthik <secharank@gmail.com>
2022-10-08 22:29:05 -0700

    added more views

---
+++ client/src/components/Dream.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dream = (props) => {
+  return (
+    <div>Dream</div>
+  )
+}
+
+export default Dream
---
+++ client/src/views/OneDream.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OneDream = (props) => {
+  return (
+    <div>OneDream</div>
+  )
+}
+
+export default OneDream
