7a13d5accb9e8c29019ed0c5708945f5d4d749a7
Charan Karthik <secharank@gmail.com>
2022-10-18 14:33:34 -0700

    user controller started

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,11 @@
+const User = require('../models/user.model')
+const bcrypt = require('bcrypt')
+// will need to require jwt here as well
+
+module.exports.register = (req, res) => {
+
+}
+
+module.exports.login = (req, res) => {
+
+}
