27af05364661fed9c0c619dee66e9bd04c46a6bb
Charan Karthik <secharank@gmail.com>
2022-10-18 14:31:45 -0700

    user model completed

---
+++ server/models/user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        validate: {
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email"
+        }
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be at least 8 characters long"]
+    }
+})
+
+UserSchema.virtual('confirmPassword')
+    .get(() => this._confirmPassword)
+    .set(value => this._confirmPassword = value)
+
+UserSchema.pre('validate', function (next) {
+    if (this.password !== this.confirmPassword) {
+        this.invalidate('confirmPassword', 'Passwords must match');
+    }
+    next();
+});
+
+UserSchema.pre('save', function (next) {
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+            this.password = hash;
+            next();
+        });
+});
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User;
