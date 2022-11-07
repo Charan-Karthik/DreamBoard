3e05fbbab0917cb3371d6422ef64a4ffd2c7afa6
Charan Karthik <secharank@gmail.com>
2022-11-06 22:49:24 -0800

    unfunctional method removed

--- server/controllers/dream.controller.js
+++ server/controllers/dream.controller.js
@@ -15,15 +15,6 @@ module.exports.getAllPublicDreams = (req, res) => {
         .catch(err => res.json({message: "Something went wrong", error: err}))
 }
 
-module.exports.getAllDreamsByPoster = (req, res) => {
-    Dream.find()
-        .then(allDreams => {
-            const dreamsByUser = allDreams.filter(dream => dream.poster === req.params.poster)
-            return res.json(dreamsByUser)
-        })
-        .catch(err => res.json({message: "Something went wrong", error: err}))
-}
-
 module.exports.createDream = (req, res) => {
     Dream.create(req.body)
         .then(newDream => res.json(newDream))
