14a52982da35318e06f941cc2159b34f9cc1f1d5
Charan Karthik <secharank@gmail.com>
2022-12-21 13:17:24 -0800

    updated to match login (basic foundation)

--- client/src/views/Register.jsx
+++ client/src/views/Register.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import NoUserHeader from '../components/NoUserHeader'
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <div>
+            <NoUserHeader />
+        </div>
     )
 }
 
